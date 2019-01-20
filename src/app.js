ffb4116aca9c422f94f2f71cfd61198300905252
syczs <975563307@qq.com>
2019-01-20 23:25:26 +0800

    1-2完成登陆页面 注册页面

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const reister = require(path.join(__dirname,'./routers/accunRouter.js'))
 
-app.get('/' ,(req,res)=>{
-            res.send('hello world')
-})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+//section
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+//设置静态资源
+app.use(express.static(path.join(__dirname,"public")))
 
+
+app.use('/account',reister)
 
 
 app.listen(3000,'127.0.0.1',err=>{
@@ -13,3 +25,6 @@ app.listen(3000,'127.0.0.1',err=>{
     }
     console.log('satrt ok')
 })
+
+
+
